32b0cd0f990c2b9261cc266c2aa96a5aa84b22e4
Shiweiliuiiiiiii <s.liu3@tue.nl>
2024-10-29 19:53:14 +0000

    VIT TINY

--- run_tiny.sh
+++ run_tiny.sh
@@ -7,7 +7,7 @@
 # --output_dir ./save_results
 torchrun --nproc_per_node=2 --master_port=29500 main.py \
 --model vit --drop_path 0.1 \
---batch_size 512 --lr 4e-3 --update_freq 4 \
+--batch_size 1024 --lr 4e-3 --update_freq 2 \
 --model_ema true --model_ema_eval true \
 --data_path /scratch/shiwei/data/imagenet1k/ \
 --epochs 120 \
